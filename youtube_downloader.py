0a2806129f30d5faeb50d45ab3bfc9fe6c75c7b8
Geek vs Critic <33985056+GeekvsCritic@users.noreply.github.com>
2021-01-03 19:54:22 +0530

    Command line included, instead of popups.

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,50 +1,36 @@
 #!/usr/bin/env python
 
-import progressbar
-import numpy as np
-import urllib
-import re
-import pytube
 import os
 import sys
+import re
+import pytube
+from pytube.cli import on_progress
+import getopt
+import numpy as np
 # os.system(f"{sys.executable} -m pip install -U progressbar")
 # os.system(f"{sys.executable} -m pip install -U pytube")
 
-version = 1.3
+version = 1.5
 name = 'youtube_downloader.py'
-
-pbar = None
 
 
 def usage():
     sys.stderr.write(f"""
-This is {name} version {version}, a tool to download video/audio from youtube of selected quality.\n\n
-Usage: {name} -y url [-v resolution] [-a quality] [-h]\n\n
--h: Display help\n
--v: Provide the resolution you want to download ['2160p', '1440p', '1080p', '720p', '480p', '360p', '240p', '144p']\n
--a: Provide the quality you want to download ['256kbps', '128kbps', '160kbps', '70kbps', '50kbps']\n
+This is {name} version {version}, a tool to download video/audio from youtube of selected quality.\n
+Usage: {name} [url] [-v] [-a] [-q quality] [-h]\n
+Positional Parameters:
+url: The YouTube /watch or /playlist url
+-v: Provide the resolution you want to download ['2160p', '1440p', '1080p', '720p', '480p', '360p', '240p', '144p']
+-a: Provide the quality you want to download ['256kbps', '128kbps', '160kbps', '70kbps', '50kbps']
+-h: Display help
     """)
-
-
-def show_progress(block_num, block_size, total_size):
-    # https://stackoverflow.com/a/46825841/5734121
-    global pbar
-    if pbar is None:
-        pbar = progressbar.ProgressBar(maxval=total_size)
-
-    downloaded = block_num * block_size
-    if downloaded < total_size:
-        pbar.start()
-        pbar.update(downloaded)
-    else:
-        pbar.finish()
-        pbar = None
 
 
 def download_file(required_file, title, av_format, available_quality):
     file_name = title + "_" + available_quality  # +"."+file_extension
-    print("File Size: "+str(round(required_file.filesize/1024/1024, 2))+" MB")
-    file = required_file.download(filename=file_name)
+    file_size = required_file.filesize
+    print("File Size: "+str(round(file_size/1024/1024, 2))+" MB")
+    file = required_file.download(filename=file_name, output_path="./downloads/")
     file_without_ext, file_ext = os.path.splitext(file)
     os.rename(file.replace("\\", r"\\"), file_without_ext +
               ".mp3" if av_format == 'audio' and file_ext == '.mp4' else file_without_ext+file_ext)
@@ -60,6 +46,7 @@ def download_type(video, title, av_format, resolution_to_download):
     nearest_quality = (np.abs(np.asarray(numeric_quality) -
                               int(re.split('p|kbps', resolution_to_download, 0)[0]))).argmin()
     available_quality = list_of_quality[nearest_quality]
+    print("Available Quality closet to your selection: ", available_quality)
     if av_format == 'video':
         required_file = download_type.filter(res=available_quality).first()
     if av_format == 'audio':
@@ -69,8 +56,7 @@ def download_type(video, title, av_format, resolution_to_download):
 
 
 def single_video(url, av_format, resolution_to_download):
-    video = pytube.YouTube(url)
-    # video.register_on_progress_callback(show_progress_bar)
+    video = pytube.YouTube(url, on_progress_callback=on_progress)
     title = video.title
     video_id = video.video_id
     watch_url = video.watch_url
@@ -88,20 +74,47 @@ def plalist_url(url, av_format, resolution_to_download):
         single_video(video, av_format, resolution_to_download)
 
 
-def main():
-    url_input = input("Please provide the url you want to download: ")
-    av_format = input('You want to download a video/audio (v/a)? ')
-    av_format = 'video' if av_format.upper().find("V") >= 0 else 'audio'
-    quality_dict = {'video': ['2160p', '1440p', '1080p', '720p', '480p', '360p',
-                              '240p', '144p'], 'audio': ['256kbps', '128kbps', '160kbps', '70kbps', '50kbps']}
-    resolution_to_download = input(
-        f"Please enter the quality in which you want file to be downloaded, from the following: {quality_dict['video'] if av_format == 'video' else quality_dict['audio'] } ")
-
+def main(url_input, av_format, download_quality):
     if url_input.find('list') > 1:
-        plalist_url(url_input, av_format, resolution_to_download)
+        plalist_url(url_input, av_format, download_quality)
     else:
-        single_video(url_input, av_format, resolution_to_download)
+        single_video(url_input, av_format, download_quality)
+
+
+def input():
+    download_quality = None
+    argv = sys.argv[1:]
+
+    try:
+        re.search('youtu\\.be|youtube', argv[0]).start()
+        url_input = argv[0]
+    except Exception:
+        sys.stderr.write("Please provide a valid youtube link")
+
+    if argv[1] == '-v':
+        av_format = 'video'
+    if argv[1] == '-a':
+        av_format = 'audio'
+
+    try:
+        opts, args = getopt.getopt(argv[2:], 'q:h:avqh')
+    except Exception:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit(0)
+        if opt == '-q':
+            download_quality = arg
+
+    if len(argv) == 0:
+        usage()
+        sys.exit(2)
+
+    main(url_input, av_format, download_quality)
 
 
 if __name__ == "__main__":
-    main()
+    input()
